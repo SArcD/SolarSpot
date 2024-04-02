55b137aef0f1a1c60e254a92387c6eed6875b790
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-01 18:55:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -299,6 +299,28 @@ def main():
 
 
 
+                #def detectar_disco_solar(imagen):
+                    
+                    # Convertir la imagen a escala de grises
+                 #   imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+                    # Aplicar umbral adaptativo con parámetros ajustados
+                #    thresh = cv2.adaptiveThreshold(imagen_gris, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 10)
+
+                    # Encontrar contornos en la imagen umbralizada
+                
+                 #   contornos, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                    # Seleccionar el contorno más grande (el disco solar)
+ #                   contorno_disco_solar = max(contornos, key=cv2.contourArea)
+#
+                    # Calcular el centro del contorno
+  #                  M = cv2.moments(contorno_disco_solar)
+   #                 centro_x = int(M["m10"] / M["m00"])
+    #                centro_y = int(M["m01"] / M["m00"])
+#
+ #                   return (centro_x, centro_y)
+
                 def detectar_disco_solar(imagen):
                     
                     # Convertir la imagen a escala de grises
@@ -314,35 +336,31 @@ def main():
                     # Seleccionar el contorno más grande (el disco solar)
                     contorno_disco_solar = max(contornos, key=cv2.contourArea)
 
-                    # Calcular el centro del contorno
-                    M = cv2.moments(contorno_disco_solar)
-                    centro_x = int(M["m10"] / M["m00"])
-                    centro_y = int(M["m01"] / M["m00"])
-
-                    return (centro_x, centro_y)
+                    return contorno_disco_solar
+                
 
                 # Ejemplo de uso:    
-                imagen = cv2.imread('imagen.jpg')
-                posicion_disco_solar = detectar_disco_solar(imagen)    
-                st.write("Posición del centro del disco solar:", posicion_disco_solar)
+                #imagen = cv2.imread('imagen.jpg')
+                #posicion_disco_solar = detectar_disco_solar(imagen)    
+                #st.write("Posición del centro del disco solar:", posicion_disco_solar)
 
 
                 
 
         
                 # Cargar la imagen desde el archivo cargado
-                #image = Image.open(uploaded_file)
+                image = Image.open(uploaded_file)
                 #    Convertir la imagen RGB a formato BGR
-                #image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
                 # Detectar el disco solar en la imagen
-                #contorno_disco_solar = detectar_disco_solar(image_bgr)
+                contorno_disco_solar = detectar_disco_solar(image_bgr)
                 # Dibujar el contorno del disco solar en la imagen
-                #cv2.drawContours(image_bgr, [contorno_disco_solar], -1, (0, 255, 0), 2)
+                cv2.drawContours(image_bgr, [contorno_disco_solar], -1, (0, 255, 0), 2)
                 # Convertir la imagen de nuevo a formato compatible con Streamlit
-                #image_with_text = Image.fromarray(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB))
-                #st.write("Esta es tu foto del Sol:")
+                image_with_text = Image.fromarray(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB))
+                st.write("Esta es tu foto del Sol:")
                 # Mostrar la imagen con texto y contorno del disco solar
-                #st.image(image_with_text, caption="Fotografía del Sol durante el eclipse", use_column_width=True)
+                st.image(image_with_text, caption="Fotografía del Sol durante el eclipse", use_column_width=True)
                 
 
 if __name__ == "__main__":
