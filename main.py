c8ee313d07770cc369a624766d5a81813d67283c
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-01 19:17:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -329,36 +329,6 @@ def main():
                 # Mostrar la imagen con texto y contorno del disco solar
                 st.image(image_with_text, caption="Fotografía del Sol durante el eclipse", use_column_width=True)
                 
-                import cv2
-                import numpy as np
-
-                def detectar_disco_solar(imagen):
-                    # Convertir la imagen a escala de grises
-                    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-
-                    # Aplicar umbral adaptativo con parámetros ajustados
-                    thresh = cv2.adaptiveThreshold(imagen_gris, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 10)
-
-                    # Encontrar contornos en la imagen umbralizada
-                    contornos, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                    # Seleccionar el contorno más grande (el disco solar)
-                    contorno_disco_solar = max(contornos, key=cv2.contourArea)
-
-                    # Encontrar el círculo que encierra el contorno
-                    (x, y), radio = cv2.minEnclosingCircle(contorno_disco_solar)
-                    centro_x = int(x)
-                    centro_y = int(y)
-
-
-                    return contorno_disco_solar
-
-                # Ejemplo de uso:
-                imagen = cv2.imread('imagen.jpg')
-                posicion_disco_solar = detectar_disco_solar(imagen)
-                st.image("Posición del centro del disco solar:", posicion_disco_solar)
-                st.image(image_with_text, caption="Fotografía del Sol durante el eclipse", use_column_width=True)
-
 
                 # Convertir la imagen RGB a formato BGR
                 #image_bgr = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
