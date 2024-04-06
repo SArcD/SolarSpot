ef4bd9833d081417001ccf352aca723cb3a64d04
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-06 15:19:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,9 @@ def main():
                 font_color = (255, 255, 255)
                 line_type = 1
 
-                cv2.line(imagen_con_circulo, centro_sol, (image_np.shape[1], centro_sol[1]), (255, 255, 255), 1)
+                #cv2.line(imagen_con_circulo, centro_sol, (image_np.shape[1], centro_sol[1]), (255, 255, 255), 1)
+                cv2.line(imagen_con_circulo, centro_sol, rightmost_point, (255, 255, 255), 1)
+
 
                 cv2.putText(imagen_con_circulo, f"Autor: {autor}", bottom_left_corner, font, font_scale, font_color, line_type, cv2.LINE_AA)
                 cv2.putText(imagen_con_circulo, f"Lugar: {lugar}", (bottom_left_corner[0], bottom_left_corner[1] - 30), font, font_scale, font_color, line_type, cv2.LINE_AA)
