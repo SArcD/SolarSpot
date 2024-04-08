2e3d61e813f0ebd62e274901f8d78ed0e913fdcd
SArcD <141703135+SArcD@users.noreply.github.com>
2024-04-07 22:33:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -733,12 +733,51 @@ def main():
         folium.PolyLine(locations=coordinates_sur, color='cyan').add_to(mexico_map)
         folium.PolyLine(locations=coordinates_norte, color='red').add_to(mexico_map)
 
+        def calculate_partiality_80(coordinates):
+            partiality_80 = []
+
+            # Iterar a través de cada par de coordenadas
+            for i in range(len(coordinates) - 1):
+                lat1, lon1 = coordinates[i]
+                lat2, lon2 = coordinates[i + 1]
+
+                # Calcular la distancia entre los puntos
+                # Aquí puedes usar la fórmula de la distancia entre dos puntos en la superficie de la Tierra
+                # O utilizar alguna biblioteca de Python como geopy para calcular la distancia
+                distance = calculate_distance(lat1, lon1, lat2, lon2)
+
+                # Dividir la distancia por 10
+                distance_partial = distance / 10
+
+                # Multiplicar por 4 para obtener la distancia correspondiente al 80% de la parcialidad
+                distance_partial_80 = distance_partial * 4
+
+                # Calcular las coordenadas de los puntos intermedios
+                lat_partial_80 = lat1 + (lat2 - lat1) * distance_partial_80 / distance
+                lon_partial_80 = lon1 + (lon2 - lon1) * distance_partial_80 / distance
+
+                # Agregar las coordenadas a la lista de parcialidad al 80%
+                partiality_80.append((lat_partial_80, lon_partial_80))
+
+            return partiality_80
+
+## Ejemplo de uso con las coordenadas del borde norte de la zona de totalidad
+#coordinates_norte = [
+#    (6 + (11.7 / 60), -(146 + (38.0 / 60))), (5 + (8.4 / 60), -(143 + (0.6 / 60))),
+#    # Resto de las coordenadas...
+#]
+
+        partiality_80_norte = calculate_partiality_80(coordinates_norte)
+        #print(partiality_80_norte)
+        partiality_80_sur = calculate_partiality_80(coordinates_sur)
+
         # Agregar la línea poligonal al mapa
         #folium.PolyLine(locations=[(lat, -abs(deg) - (min / 60)) for deg, min, _, _ in coordinates], color='blue').add_to(mexico_map)
 
         # Mostrar el mapa
         #mexico_map
-
+        folium.PolyLine(locations=partiality_80_norte, color='cyan').add_to(mexico_map)
+        folium.PolyLine(locations=partiality_80_sur, color='cyan').add_to(mexico_map)
 
 
         # Mostrar el mapa en Streamlit
